7b1748533d713c6bfd8ea950eb630ed33b53d7b7
petrov826 <g8myc3hjz@me.com>
2024-04-20 11:52:00 +0900

    create 3x3 board

--- src/App.js
+++ src/App.js
@@ -1,3 +1,26 @@
-export default function Square() {
-  return <button className="square">X</button>;
+// Squareコンポーネントを宣言する
+// export: この関数を他のファイルからアクセスできるようにする
+// default: この関数を使うファイルに、この関数がメイン関数だと伝える
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </>
+  );
 }

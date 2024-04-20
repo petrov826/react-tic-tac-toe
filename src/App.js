2d46ca882d35ab40f64b7e048197e822a1bd230b
petrov826 <g8myc3hjz@me.com>
2024-04-20 11:59:36 +0900

    use props to display different value

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-function Square() {
-  return <button className="square">1</button>
+function Square({ value }) {
+  // propsで受け取った値を表示する
+  return <button className="square">{value}</button>
 }
 
 // Squareコンポーネントを宣言する
@@ -9,21 +10,21 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
 
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
 
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </>
   );

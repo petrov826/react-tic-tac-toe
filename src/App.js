2ca019dee4a7f2f4e8a806e2b545018b56b9bd4e
petrov826 <g8myc3hjz@me.com>
2024-04-20 11:55:43 +0900

    refactor repeatitive code with new component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+function Square() {
+  return <button className="square">1</button>
+}
+
 // Squareコンポーネントを宣言する
 // export: この関数を他のファイルからアクセスできるようにする
 // default: この関数を使うファイルに、この関数がメイン関数だと伝える
@@ -5,21 +9,21 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
 
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
 
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );

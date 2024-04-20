f99b63e245077cd03dd8f0f8a066939352f7f55f
petrov826 <g8myc3hjz@me.com>
2024-04-20 12:03:51 +0900

    use useState to manage value instead of using props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-function Square({ value }) {
+import { useState } from "react";
+
+function Square() {
+  const [value, setValue] = useState(null)
+
   function handleClick() {
-    console.log("clicked")
+    setValue("X")
   }
 
   // propsで受け取った値を表示する
@@ -14,21 +18,21 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
 
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
 
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
     </>
   );
